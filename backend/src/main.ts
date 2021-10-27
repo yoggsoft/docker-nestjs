fe5639a07f569cb1406580180b8c9944562c02fd
yoggsoft <yoggsoft@gmail.com>
2021-10-28 00:01:38 +0200

    add swagger and movies resource

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Docker Nest')
+    .setDescription('Docker running NestJS REST API')
+    .setVersion('1.0.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('/', app, swaggerDocument);
+
+  await app.listen(3030);
 }
 bootstrap();
---
+++ backend/src/movie/movie.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Injectable()
+export class MovieService {
+  create(createMovieDto: CreateMovieDto) {
+    return 'This action adds a new movie';
+  }
+
+  findAll() {
+    return `This action returns all movie`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} movie`;
+  }
+
+  findByTitle(title: string) {
+    return `This action returns the movie #${title}`;
+  }
+
+  update(id: number, updateMovieDto: UpdateMovieDto) {
+    return `This action updates a #${id} movie`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} movie`;
+  }
+}
